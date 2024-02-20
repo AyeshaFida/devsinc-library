33a4f07cc27ef8304e75258d19fd314e372d3237
AyeshaFida <ayesha.fidahussain@devsinc.com>
2024-02-20 17:42:35 +0500

    updated assets

--- component-library/storybook-static/assets/Introduction-8dee6748.js
+++ component-library/storybook-static/assets/Introduction-8dee6748.js
@@ -1,4 +1,4 @@
-import{a as i,j as r,F as a}from"./jsx-runtime-03b4ddbf.js";import{M as s}from"./index-a8b4ca86.js";import{u as o}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-0b741434.js";import"../sb-preview/runtime.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-ec3b3d19.js";import"./index-d3ea75b5.js";import"./index-11d98b33.js";import"./index-8fd8397b.js";import"./index-356e4a49.js";function n(e){const t=Object.assign({h1:"h1",p:"p",img:"img"},o(),e.components);return r(a,{children:[i(s,{title:"Devsinc UI Library/Introduction"}),`
+import{a as i,j as r,F as a}from"./jsx-runtime-03b4ddbf.js";import{M as s}from"./index-f823f76a.js";import{u as o}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-e264b729.js";import"../sb-preview/runtime.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-ec3b3d19.js";import"./index-d3ea75b5.js";import"./index-11d98b33.js";import"./index-8fd8397b.js";import"./index-356e4a49.js";function n(e){const t=Object.assign({h1:"h1",p:"p",img:"img"},o(),e.components);return r(a,{children:[i(s,{title:"Devsinc UI Library/Introduction"}),`
 `,i("style",{children:`
     # {
       font-family: 'Rubik', sans-serif !important;
@@ -114,5 +114,5 @@ import{a as i,j as r,F as a}from"./jsx-runtime-03b4ddbf.js";import{M as s}from".
     }
   `}),`
 `,i(t.h1,{id:"devsinc-ui-library",children:"Devsinc UI Library"}),`
-`,i(t.p,{children:i(t.img,{src:"./public/component-library.png",alt:"Image"})}),`
+`,i(t.p,{children:i(t.img,{src:"component-library.png",alt:"Image"})}),`
 `,r(t.p,{children:["Devsinc UI Library, is a specialized user interface (UI) library that has been developed by ",i("b",{children:"RAPP Team"})," for internal use within all Devsinc's products. By utilizing this custom react based library, Devsinc is able to enhance the functionality and aesthetic appeal of their products while maintaining a high level of consistency and efficiency in their development process."]})]})}function k(e={}){const{wrapper:t}=Object.assign({},o(),e.components);return t?i(t,Object.assign({},e,{children:i(n,e)})):n(e)}export{k as default};
